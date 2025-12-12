f0fea731836e79e2ba0fdb111904c89365c91b6e
Yael <alary.yael@gmail.com>
2025-12-12 22:32:39 +0100

    Added first scroll to bottom of the grid

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
+<template>
+  <GridLayout v-model:layout="layout" :col-num="200" :row-height="50" :is-draggable="true" :is-resizable="false"
+    :is-mirrored="false" :vertical-compact="false" :prevent-collision="true" :margin="[0, 0]" :use-css-transforms="true"
+    class="grid-container">
+    <GridItem v-for="item in layout" :key="item.i" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i"
+      class="select-none">
+      <img v-if="item.sprite && sprites[item.sprite]" :src="sprites[item.sprite]" class="sprite-img"
+        :alt="item.sprite" />
+      <span v-else class="text">{{ item.i }}</span>
+    </GridItem>
+  </GridLayout>
+  <div v-if="!clickedScroll" @click="scrollToBottom"
+    class="h-20 w-20 bg-blue-200 border-[4px] border-blue-800 rounded-full top-[calc(100vh/2)] left-[calc(100vw/2)] translate-x-[-50%] translate-y-[-50%] fixed z-[9999] flex items-center justify-center cursor-pointer hover:bg-blue-300">
+    <ArrowDownIcon class="h-10 w-10 text-blue-800 m-auto" />
+  </div>
+  <div id="spLib" class="h-[200px]"></div>
+  <div id="test"></div>
+</template>
+
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 import { GridLayout, GridItem } from 'vue-grid-layout-v3';
+import { ArrowDownIcon } from '@heroicons/vue/24/solid'
 
 import grassBlock1 from './assets/sprites/grass_block_1.png';
 import grassObj1 from './assets/sprites/grass_obj_1.png';
@@ -19,29 +39,21 @@ const sprites = {
 };
 
 const layout = ref([
-  { "x": 0, "y": 200, "w": 1, "h": 1, "i": "0", "sprite": "mushroom_block_1" },
+  { "x": -1, "y": 100, "w": 1, "h": 1, "i": "1", "sprite": "mushroom_block_1" }, //USED FOR FIXING GRID, SHOULD BE REMOVED AT THE END
+  { "x": 0, "y": 100, "w": 1, "h": 1, "i": "0", "sprite": "mushroom_block_1" },
+
 ]);
 
-onMounted(() => {
-  window.scrollTo(0, document.body.scrollHeight);
-});
-</script>
+const clickedScroll = ref(false);
 
-<template>
-  <GridLayout v-model:layout="layout" :col-num="200" :row-height="50" :is-draggable="true" :is-resizable="false"
-    :is-mirrored="false" :vertical-compact="false" :prevent-collision="true" :margin="[0, 0]" :use-css-transforms="true"
-    class="grid-container">
-    <GridItem v-for="item in layout" :key="item.i" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i">
-      <img v-if="item.sprite && sprites[item.sprite]" :src="sprites[item.sprite]" class="sprite-img"
-        :alt="item.sprite" />
-      <span v-else class="text">{{ item.i }}</span>
-    </GridItem>
-  </GridLayout>
-  <!-- <div
-    class="h-20 w-20 bg-blue-200 border-[4px] border-blue-500 rounded-full top-[calc(100vh/2)] left-[calc(100vw/2)] translate-x-[-50%] translate-y-[-50%] fixed z-[9999]">
-  </div>
-  <div class="min-h-[200px]"></div> -->
-</template>
+const scrollToBottom = () => {
+  clickedScroll.value = true;
+  const spLib = document.getElementById('spLib');
+  if (spLib) {
+    spLib.scrollIntoView({ block: 'end' });
+  }
+};
+</script>
 
 <style>
 body,
@@ -63,14 +75,10 @@ html {
   min-height: 200px;
   height: auto;
   background-image:
-    linear-gradient(to right, #999 1px, transparent 1px),
-    linear-gradient(to bottom, #999 1px, transparent 1px);
+    linear-gradient(to right, #ccc 1px, transparent 1px),
+    linear-gradient(to bottom, #ccc 1px, transparent 1px);
   background-size: 50px 50px;
   background-position: 0 0;
-}
-
-.vue-grid-item:not(.vue-grid-placeholder) {
-  border: 1px solid black;
 }
 
 .vue-grid-item .text {
